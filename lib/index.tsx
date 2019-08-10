4a0c8b9fa590601dc7c3bfbdb679658a4b22be92
xuanzebin <xuanzebin@126.com>
2019-08-10 14:54:19 +0800

    feat: 终于把配置文件配置成功了

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,12 +1,6 @@
+import Button from './button'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function Button(){
-    return (
-        <div>
-            按钮
-        </div>
-    )
-}
 
 ReactDOM.render(<Button />, document.body)
---
+++ test/setupTests.js
@@ -0,0 +1,4 @@
+const enzyme = require('enzyme')
+const Adapter = require('enzyme-adapter-react-16.3')
+
+enzyme.configure({adapter: new Adapter()})
