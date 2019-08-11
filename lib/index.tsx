4efa802fccd9959fc547483103ef3716ac51f4e6
xuanzebin <xuanzebin@126.com>
2019-08-11 13:23:00 +0800

    feat: 新建了Icon组件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,6 +1,10 @@
-import Button from './button'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Icon from './Icon'
 
-ReactDOM.render(<Button />, document.body)
+ReactDOM.render(
+<div>
+    <Icon name="jd"/>
+</div>
+, document.querySelector('#root'))
